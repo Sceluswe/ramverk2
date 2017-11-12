b336b7bd95967a0bb17f31b65f28999480f5b459
Sceluswe <emilcbm@gmail.com>
2017-11-12 21:03:44 +0100

    added boostrap and gulp

--- redovisa/index.js
+++ redovisa/index.js
@@ -64,91 +64,7 @@ app.get("/about", (req, res) => {
     });
 });
 
-
-// ---------------- default test routes ----------------
-// Add a route
-
-
-// Testing routes with method
-// app.get("/user", (req, res) => {
-    // res.send("Got a GET request at " + req.originalUrl);
-// });
-
-// app.post("/user", (req, res) => {
-    // res.send("Got a POST request at " + req.originalUrl);
-// });
-
-// app.put("/user", (req, res) => {
-    // res.send("Got a PUT request at " + req.originalUrl);
-// });
-
-// app.delete("/user", (req, res) => {
-    // res.send("Got a DELETE request at " + req.originalUrl);
-// });
-
-// Add a saying hello
-// app.get("/hello", (req, res) => {
-    // res.send("Hello World");
-// });
-
-// Route with dynamic content save in a parameter
-// app.get("/hello/:message", (req, res) => {
-    // res.send(req.params.message);
-// });
-
-// Route that renders template view.
-// app.get("/test/page", (req, res) => {
-    // res.render("page", {
-        // title: "Hey",
-        // message: "Hello there!"
-    // });
-// });
-
-// app.get("/test/:title/:message", (req, res) => {
-    // res.render("page", {
-        // title: req.params.title,
-        // message: req.params.message
-    // });
-// });
-
-// app.get("/test/home", (req, res) => {
-    // res.render("home", {
-        // title: "Home"
-    // });
-// });
-
-// app.get("/test/blog", (req, res) => {
-    // res.render("blog", {
-        // title: "Blog",
-        // posts: [
-            // {
-                // title: "Blog post 1",
-                // content: "Content 1."
-            // },
-            // {
-                // title: "Blog post 2",
-                // content: "Content 2."
-            // },
-            // {
-                // title: "Blog post 3",
-                // content: "Content 3."
-            // },
-        // ]
-    // });
-// });
-
-// app.get("/test/markdown", (req, res) => {
-    // res.render("markdown", {
-        // title: "Markdown"
-    // });
-// });
-
-// app.get("/test/markdown-include", (req, res) => {
-    // res.render("markdown-include", {
-        // title: "Markdown include"
-    // });
-// });
-
+// ---------------- Error handlers ----------------
 // Note the error handler takes four arguments
 app.use((err, req, res, next) => {
     if (res.headersSent) {
@@ -164,9 +80,10 @@ app.use((err, req, res, next) => {
 // Add routes for 404 and error handling
 // Catch 404 and forward to error handler
 // Put this last
-// app.use((req, res, next) => {
-// var err = new Error("Not Found");
-// err.status = 404;
-// next(err);
-// });
+app.use((req, res, next) => {
+    var err = new Error("Not Found");
+
+    err.status = 404;
+    next(err);
+});
 
